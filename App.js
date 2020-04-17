2ee074b6e3a2abca539d5b7cc70b3a6bb5d4e1b4
Lindsay Ferreira <eson@iouu.com.br>
2020-04-17 16:39:49 -0300

    estrutura inicial

---
+++ App.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Modal, Image } from 'react-native';
+
+import { camera, Camera } from 'expo-camera'
+
+import { FontAwesome } from '@expo/vector-icons'
+
+export default function App() {
+	const camRef = useRef(null)
+	const [type, setType] = useState(Camera.Constants.Type.back)
+	const [hasPermission, setHaspermision] = useState(null)
+	const [capturedPhoto, setCapturedPhoto] = useState(null)
+	const [open, setOpen] = useState(false)
+
+
+	useEffect(() => {
+		(async () => {
+			const { status } = await Camera.requestPermissionsAsync()
+			setHaspermision(status === 'granted')
+		})()
+	}, [])
+
+	if (hasPermission === null) {
+		return <View></View>
+	}
+
+	if (hasPermission === false) {
+		return <Text>Acesso negado</Text>
+	}
+
+	async function takePicture() {
+		if (camRef) {
+			const data = await camRef.current.takePictureAsync()
+			setCapturedPhoto(data.uri)
+			setOpen(true)
+		}
+	}
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<Camera
+				style={{ flex: 2}}
+				type={type}
+				ref={camRef}
+			/>
+			<View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}></View>
+			<TouchableOpacity
+				style={{
+					flex: 1,
+					position: 'absolute',
+					bottom: 20,
+					left: 20
+
+				}}
+
+				onPress={() => {
+					setType(
+						type === Camera.Constants.Type.back
+							? Camera.Constants.Type.front
+							: Camera.Constants.Type.back
+					)
+				}}
+			>
+				<Text style={{ fontSize: 20, marginBottom: 13, color: '#FFF' }} > Trocar</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity style={styles.button} onPress={takePicture}>
+				<FontAwesome name='camera' size={23} color='#fff' />
+			</TouchableOpacity>
+
+			{
+				capturedPhoto &&
+				<Modal
+					animationType='slide'
+					transparent={false}
+					visible={open}
+				>
+					<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', margin: 20 }}>
+
+						<TouchableOpacity style={{ margin: 10 }} onPress={() => setOpen(false)}>
+							<FontAwesome name="window-close" size={50} color="#FF0000" />
+
+						</TouchableOpacity>
+
+						<Image
+							style={{ width: '100%', height: 300, borderRadius: 15 }}
+							source={{ uri: capturedPhoto }}
+						/>
+
+					</View>
+
+				</Modal>
+			}
+
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+	},
+	button: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#121212',
+		margin: 20,
+		borderRadius: 10,
+		height: 50,
+	}
+});
